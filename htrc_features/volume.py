83346c8d755aa2a5cbd69f4c17c5c7c139cd8e03
Peter Organisciak <organisciak@gmail.com>
2014-05-22 17:38:04 -0500

    Adding processing methods into Volume class. Log:
     - add Volume method for getting a list of token counts across the whole volume
    
     - removed python 2 specific .iteritems
    
     - Spin off TermIndex into its own module
    
     - Add Volume.frequencies() for a term-page frequency (or occurrance) index
    
    Add term_page and term_volume frequency methods for Volume

--- htrc_features/volume.py
+++ htrc_features/volume.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from htrc_features.page import Page, Section
+from htrc_features.term_index import TermIndex
 from six import iteritems
 import logging
 
@@ -17,7 +18,7 @@ class Volume(object):
         self.pageCount = obj['features']['pageCount']
 
         # Expand metadata to attributes
-        for (key, value) in obj['metadata'].iteritems():
+        for (key, value) in obj['metadata'].items():
             setattr(self, key, value)
         self.pageindex = 0
     
@@ -41,6 +42,39 @@ class Volume(object):
     def pages(self):
         for page in self._pages:
             yield Page(page, self)
+
+    def tokens_per_page(self):
+        l = [0] * self.pageCount
+        for (index, page) in enumerate(self.pages()):
+            try:
+                l[index] = page.total_tokens()
+            except:
+                logging.error("Seq and pageCount don't match in %s" % self.id)
+        return l
+
+    def term_page_freqs(self, page_freq=True, case=False):
+        return self._frequencies(page_freq, case)
+
+    def term_volume_freqs(self, page_freq=True, case=False):
+        tp = self._frequencies(page_freq, case)
+        for (token, l) in iteritems(tp):
+            tp[token] = sum(l)
+        return tp
+
+    def _frequencies(self, page_freq=True, case=False):
+        ''' Build term-page frequency list.
+
+        page_freq[bool] : Whether to count page frequency(if the term occurs in the page or not)
+                          or a full frequency.
+        '''
+        ti = TermIndex(self.pageCount)
+        for (index, page) in enumerate(self.pages()):
+            for (token, count) in iteritems(page.tokenlist.token_counts(case=case, pos=False)):
+                if page_freq:
+                    ti[token][index] = 1
+                else:
+                    ti[token][index] = count
+        return ti
 
     def __str__(self):
         return "<HTRC Volume: %s>" % self.id
