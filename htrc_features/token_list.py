71b9a054b94790b3b5edc9ae5bcae37d70ee218e
Peter Organisciak <organisciak@gmail.com>
2015-12-15 19:33:25 -0700

    Gut tokenlist to use Pandas, and return DataFrames

--- htrc_features/token_list.py
+++ htrc_features/token_list.py
@@ -1,65 +1,66 @@
 from __future__ import unicode_literals
 from collections import defaultdict
-from six import iteritems
-import htrc_features.utils as utils
+import pandas as pd
+import logging
+
 
 class TokenList(object):
 
     def __init__(self, tokendict={}):
         self._rawtokens = defaultdict(lambda: defaultdict(int))
 
-        for (t, poscounts) in iteritems(tokendict):
-            self.add(t, poscounts)
+        # Make token list into a long Pandas DataFrame
+        wide_df = pd.DataFrame(tokendict).transpose().reset_index()\
+                    .rename(columns={"index": "token"})
+        self._df = pd.melt(wide_df, id_vars=['token'], var_name="POS",
+                           value_name="count").dropna()
 
     def token_counts(self, case=True, pos=True):
-        tc = self._token_counts(case=case, pos=pos)
-        # Since _token_counts is a toekn specific iterator, any casefold needs
-        # to be done here
-        if case is True:
-            return dict(tc)
-        else:
-            if pos is True:
-                folded = self._merge_duplicates(tc)
-            else:
-                folded = defaultdict(int)
-                for (t,c) in tc:
-                    assert(type(c)==int)
-                    folded[t] += c
-            return dict(folded)
-
-    def _token_counts(self, case=True, pos=True):
-        ''' 
-        Generator for pulling (token, count) items.
-
-        Since it's a generator, duplicates are possible.
-
-        pos[bool] : Specify whether to return frequencies per part of speech, or overall
         '''
-        for (token, counts) in iteritems(self._rawtokens):
-            if case is False:
-                token = token.lower()
-            if pos is False:
-                counts = sum(counts.values())
-            yield (token, counts)
-    
-    def add(self, token, posdict):
-        ''' Add a new ( token , { pos : count } ) token to the data, merging when necessary.'''
-        for (pos, count) in iteritems(posdict):
-            self._rawtokens[token][pos] += count
+        Get counts of tokens in the tokenlist.
+
+        case[bool] : Preserve case, or fold.
+        pos[bool] : Specify whether to return frequencies per part of speech,
+                    or simply by word
+        '''
+
+        if case is True and pos is True:
+            # Specify columns explicitly to keep consistency
+            return self._df[['token', 'POS', 'count']]
+        elif case is True and pos is False:
+            return self._df.groupby('token').sum().reset_index()
+        elif case is False:
+            if 'lowercase' not in self._df.columns:
+                # We won't create lowercase versions of strings until needed
+                # But might as well keep them around
+                logging.debug('Adding lowercase column')
+                self._df['lowercase'] = self._df['token'].str.lower()
+
+            if pos is True:
+                return self._df.groupby(['lowercase', 'POS']).sum()\
+                           .reset_index()\
+                           .rename(columns={"lowercase": "token"})
+            elif pos is False:
+                return self._df.groupby('lowercase').sum().reset_index()\
+                           .rename(columns={"lowercase": "token"})
+        else:
+            logging.error("Invalid parameters for token counts")
+            return
 
     def add_TokenList(self, tokenlist):
-        for (token, posdict) in iteritems(tokenlist.token_counts()):
-            self.add(token, posdict)
-    
+        ''' Concatenate a second tokenlist '''
+
+        new_df = pd.concat([self._df, tokenlist.token_counts()])\
+                   .drop('lowercase', 1).groupby(['token', 'POS']).sum()\
+                   .reset_index()
+        self._df = new_df
+
     @property
     def tokens(self):
         ''' Get unique tokens '''
-        return list(self._rawtokens.keys())
-
-#    def _merge_duplicates(tokens):
-#        return utils.merge_token_duplicates(tokens)
+        return self._df['token'].unique().tolist()
 
     @property
-    def count():
-        counts = [count for (token, count) in self._token_counts(pos=False)]
-        return sum(counts)
+    def count(self):
+        ''' Count total tokens on the page '''
+        return self._df['count'].astype(int).sum()
