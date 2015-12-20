a3d5920da985559b96f9bff39e0696fd0e8aef01
Peter Organisciak <organisciak@gmail.com>
2015-12-20 15:18:44 -0700

    Remove section and tokenlist, refactor token and char counting in Page

--- htrc_features/__init__.py
+++ htrc_features/__init__.py
@@ -1,5 +1,5 @@
 from htrc_features.feature_reader import FeatureReader
 from htrc_features.volume import Volume
-from htrc_features.page import *
+from htrc_features.page import Page
 from htrc_features.term_index import TermIndex
 
--- htrc_features/page.py
+++ htrc_features/page.py
@@ -1,105 +1,177 @@
 from __future__ import unicode_literals
-from collections import defaultdict
-from copy import copy
-from htrc_features.token_list import TokenList
-import htrc_features.utils as utils
-from six import iteritems # Because python2's dict.iteritem is python3's dict.item
+# Because python2's dict.iteritem is python3's dict.item
+from six import iteritems
+import pandas as pd
+import logging
+
 
 class Page:
+
+    _tokencounts = pd.DataFrame()
+    _lineChars = pd.DataFrame()
+    _secref = ['header', 'body', 'footer']
+
     def __init__(self, pageobj, volume, default_section='body'):
         self.volume = volume
         self.default_section = default_section
+        self._json = pageobj
+
+        assert(self.default_section in self._secref + ['all', 'group'])
+
         for (key, item) in iteritems(pageobj):
-            setattr(self, key, pageobj[key])
+            # Only add attributes to this object if it's not overwriting
+            # a definition
+            if not hasattr(self, key):
+                setattr(self, key, pageobj[key])
 
-        for sec in ['header', 'body', 'footer']:
-            secjson = getattr(self, sec)
-            setattr(self, sec, Section(name=sec, data=secjson, page=self))
+    def tokenlist(self, section='default', case=True, pos=True):
+        ''' Get or set tokencounts DataFrame
 
-    @property
-    def tokenlist(self):
-        ''' Returns tokenList for default_section '''
-        assert(self.default_section in ['body', 'fullpage', 'header', 'footer'])
-        sec = getattr(self, self.default_section)
-        return sec.tokenlist
+        section[string]: Which part of the page to return. In addition to
+            'header', 'body', and 'footer', 'all' will return a DataFrame with
+            all the sections, 'group' will sum all sections,
+            section in ['header', 'footer', 'body'] will return those fields
+            section == 'all' will group and sum all sections
+            section == 'default' falls back on what the page object has saved
 
-    def total_tokens(self):
-        sec = getattr(self, self.default_section)
-        return sec.total_tokens()
+        case[bool] : Preserve case, or fold. To save processing, it's likely
+                    more efficient to calculate lowercase later in the process:
+                    if you want information for all pages, first collect your
+                    information case-sensitive, then fold at the end.
 
-    @property
-    def fullpage(self):
-        ''' Merge header, body, and footer ''' 
-        # Check if this has already been done
-        if hasattr(self, '_fullpage'):
-            return self._fullpage
-        
-        #newdata = {}
-        #fullpage = Section(name='fullpage', data=newdata, page=self)
-        fullpage = self._combine_sections([self.header, self.body, self.footer])
-        fullpage.name = 'full'
-        
-        # Save merged section for easy recall
-        self._fullpage = fullpage
+        pos[bool] : Specify whether to return frequencies per part of speech,
+                    or simply by word
+        '''
+        section = self.default_section if section == 'default' else section
 
-        return fullpage
+        # Create the internal representation if it does not already
+        # exist. This will only need to exist once
+        if self._tokencounts.empty:
+            if self.volume._schema == '1.0':
+                tname = 'tokens'
+            else:
+                tname = 'tokenPosCount'
+            tuples = {(int(self.seq), sec, token, pos): {'count': value}
+                      for sec in self._secref
+                      for token, posvals in iteritems(self._json[sec][tname])
+                      for pos, value in iteritems(posvals)
+                      }
+            self._tokencounts = pd.DataFrame(tuples).transpose()
+            self._tokencounts.index.names = ['page', 'section', 'token', 'pos']
 
-    def _combine_sections(self, secs):
-        # Create a copy of the first section and add the others to it
-        new_sec = copy(secs[0])
-        for sec in secs[1:]:
-            assert(isinstance(sec, Section))
-            new_sec.add_Section(sec)
-            new_sec.name =  "%s+%s" % (new_sec.name, sec.name)
-        return new_sec
+        groups = ['page']
+        if section in ['all'] + self._secref:
+            groups.append('section')
+        groups.append('token' if case else 'lowercase')
+        if pos:
+            groups.append('pos')
+
+        if section in ['all', 'group']:
+            df = self._tokencounts
+        elif section in self._secref:
+            idx = pd.IndexSlice
+            try:
+                df = self._tokencounts.loc[idx[:, section, :, :], ]
+            except KeyError:
+                logging.debug("Section {} not available".format(section))
+                df = pd.DataFrame([], columns=groups+['count'])\
+                       .set_index(groups)
+                return df
+        else:
+            logging.error("Invalid section argument: {}".format(section))
+            return
+
+        # Add lowercase column. Previously, this was saved internally. However,
+        # DataFrame.str.lower() is reasonably fast and the need to call it
+        # repeatedly is low, so it is no longer saved.
+        if not case:
+            logging.debug('Adding lowercase column')
+            df = df.reset_index()
+            df['lowercase'] = df['token'].str.lower()
+            df.set_index(['page', 'section', 'lowercase', 'token', 'pos'],
+                         inplace=True)
+
+        # Check if we need to group anything
+        if groups == ['page', 'section', 'token', 'pos']:
+            return df
+        else:
+            return df.reset_index().groupby(groups).sum()
+
+    def endLineChars(self, section='default'):
+        return self.lineChars(section=section, place='end')
+
+    def beginLineChars(self, section='default'):
+        return self.lineChars(section=section, place='begin')
+
+    def lineChars(self, section='default', place='all'):
+        '''
+        Get a dataframe of character counts at the start and end of lines
+        '''
+        section = self.default_section if section == 'default' else section
+
+        if self._lineChars.empty:
+            lineChars = {(int(self.seq), sec, place, char): {'count': value}
+                         for sec in self._secref
+                         for place in ['begin', 'end']
+                         for char, value in iteritems(
+                             self._json[sec][place+'LineChars'])
+                         }
+            self._lineChars = pd.DataFrame(lineChars).transpose()
+            names = ['page', 'section', 'place', 'character']
+            self._lineChars.index.names = names
+
+        df = self._lineChars
+
+        # Set up grouping
+        groups = ['page']
+        if section in self._secref + ['all']:
+            groups.append('section')
+        if place in ['begin', 'end', 'all']:
+            groups.append('place')
+        groups.append('character')
+
+        # Set up slicing
+        slices = [slice(None)]
+        if section in ['all', 'group']:
+            slices.append(slice(None))
+        elif section in self.secref:
+            slices.append([section])
+        if place in ['begin', 'end', 'all']:
+            slices.append([place])
+        elif place == 'group':
+            # It's hard to imagine a use for place='group', but adding for
+            # completion
+            slices.append(slice(None))
+
+        if slices != [slice(None)] * 3:
+                df = df.loc[tuple(slices), ]
+
+        if groups == ['page', 'section', 'place', 'character']:
+            return df
+        else:
+            return df.groupby(groups).sum()
+
+    def token_count(self, section):
+        ''' Count total tokens on the page '''
+        return self.tokenlist(section=section)['count'].sum()
 
     def __str__(self):
         if self.volume:
-            name="<page %s of volume %s>" % (self.seq, self.volume.id)
+            name = "<page %s of volume %s>" % (self.seq, self.volume.id)
         else:
-            name="<page %s with no volume parent>" % (self.seq)
+            name = "<page %s with no volume parent>" % (self.seq)
         return name
 
-class Section:
-    def __init__(self, name, data, page):
-        self.name = name
-        self.page = page
-        if self.page.volume._schema == '1.0':
-            self.tokenlist = TokenList(data['tokens'])
+    @property
+    def tokens(self, section='default', case=True):
+        ''' Get unique tokens '''
+        tokens = self.tokenlist(section=section).index\
+                     .get_level_values('token').to_series()
+        if case:
+            return tokens.unique().tolist()
         else:
-            self.tokenlist = TokenList(data['tokenPosCount'])
- 
-        for (key, value) in iteritems(data):
-            if type(value) is int:
-                setattr(self, key, value)
-            elif key in ['endLineChars', 'beginLineChars']:
-                # These field are saved as a defaultdict
-                setattr(self, key, defaultdict(int, value))
+            return tokens.str.lower().unique().tolist()
 
-    def add_Section(self, section):
-        ''' Add another section to this one '''
-        self.emptyLineCount += section.emptyLineCount
-        self.tokenCount += section.tokenCount
-        self.lineCount += section.lineCount
-        self.sentenceCount += section.sentenceCount
-
-        for key in ['endLineChars', 'beginLineChars']:
-            if hasattr(self, key):
-                for (char, count) in iteritems(getattr(section, key)):
-                    getattr(self, key)[char] += count
-
-        self.tokenlist.add_TokenList(section.tokenlist)
-    
-    def total_tokens(self):
-        return self.tokenCount
-        #tc = self.tokenlist.token_counts(pos=False)
-        #return sum(self.tokenlist.token_counts(pos=False).values())
-
-    def __str__(self):
-        if self.page.volume:
-            name="<%s section of page %s of volume %s>" % (self.name,
-                self.page.seq, self.page.volume.id)
-        else:
-            name="<%s section of volume-less page %s>" % (self.name,
-                self.page.seq)
-        return name
+    @property
+    def count(self):
+        return self._df['count'].astype(int).sum()
