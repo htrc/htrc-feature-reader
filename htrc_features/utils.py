6b765317eaf03b35315bcc8fa0aa88fb46fcde62
Peter Organisciak <organisciak@gmail.com>
2016-08-03 15:56:05 -0600

    Reinstate the lib-id in the filename

--- htrc_features/utils.py
+++ htrc_features/utils.py
@@ -21,12 +21,6 @@ def id_to_rsync(htid, **kwargs):
     '''
     Take an HTRC id and convert it to an Rsync location for syncing Extracted
     Features
-
-    kind: [basic|advanced]
-
-    schema: Which version of the schema. Prior to 3.0, files were
-    named with the library id (i.e. miun.blahblah.json.bz2), starting with 3.0
-    they are named without the library id (i.e. blahblah.json.bz2).
     '''
     if 'kind' in kwargs:
         logging.warn("The basic/advanced split with extracted features files "
@@ -38,7 +32,7 @@ def id_to_rsync(htid, **kwargs):
                      )
     libid, volid = htid.split('.', 1)
     volid_clean = id_encode(volid)
-    filename = '.'.join([volid_clean, 'json.bz2'])
+    filename = '.'.join([libid, volid_clean, 'json.bz2'])
     path = '/'.join([libid, 'pairtree_root', id2path(volid).replace('\\', '/'),
                      volid_clean, filename])
     return path
