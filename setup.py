81cb7e67da3020b1afec2ae2b7caa8470989f252
Peter Organisciak <organisciak@gmail.com>
2018-09-26 16:40:39 -0600

    Correct versioning error

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='htrc-feature-reader',
-      version='1.83',
+      version='1.96',
       description='Library for working with the HTRC Extracted Features dataset',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
