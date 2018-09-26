5a4630d5d78ed8397482a835a0ccf6487f48763e
Peter Organisciak <organisciak@gmail.com>
2018-09-26 12:09:22 -0600

    Make pairtree methods private and add htid sanitize method

--- htrc_features/utils.py
+++ htrc_features/utils.py
@@ -1,11 +1,31 @@
 import logging
 
-def id_encode(id):
+def _id_encode(id):
+    '''
+    :param id: A Pairtree ID. If it's a Hathitrust ID, this is the part about the library
+                code.
+    :return: A sanitized id.
+    '''
     return id.replace(":", "+").replace("/", "=").replace(".", ",")
 
 
-def id2path(id):
-    clean_id = id_encode(id)
+def clean_htid(htid):
+    '''
+    :param htid: A HathiTrust ID of form lib.vol; e.g. mdp.1234
+    :return: A sanitized version of the HathiTrust ID, appropriate for filename use.
+    '''
+    libid, volid = htid.split('.', 1)
+    volid_clean = _id_encode(volid)
+    return '.'.join([libid, volid_clean])
+
+
+def _id2path(id):
+    '''
+    :param id: Pairtree ID. For HathiTrust, only the volume id of the lib.vol id format.
+    :type id: str
+    :return: A corresponding file path for the id.
+    '''
+    clean_id = _id_encode(id)
     path = []
     while len(clean_id) > 0:
         val, clean_id = clean_id[:2], clean_id[2:]
@@ -122,7 +142,7 @@ def id_to_rsync(htid, **kwargs):
     Features.
     '''
     if 'kind' in kwargs:
-        logging.warn("The basic/advanced split with extracted features files "
+        logging.warning("The basic/advanced split with extracted features files "
                      "was removed in schema version 3.0. This function only "
                      "supports the current format for Rsync URLs, if you "
                      "would like to see the legacy 2.0 format, see Github: "
@@ -130,9 +150,9 @@ def id_to_rsync(htid, **kwargs):
                      "9ea45317443ae05f43a188b12afe2e69a/htrc_features/utils.py"
                      )
     libid, volid = htid.split('.', 1)
-    volid_clean = id_encode(volid)
-    filename = '.'.join([libid, volid_clean, 'json.bz2'])
-    path = '/'.join([libid, 'pairtree_root', id2path(volid).replace('\\', '/'),
+    volid_clean = _id_encode(volid)
+    filename = clean_htid(htid) + '.json.bz2'
+    path = '/'.join([libid, 'pairtree_root', _id2path(volid).replace('\\', '/'),
                      volid_clean, filename])
     return path
 
@@ -142,10 +162,10 @@ def htid2rsync_cmd():
     import sys
     parser = _htid2rsync_argparser()
     _htid2rsync_parse_args(parser, sys.argv[1:])
-    
+
 def _htid2rsync_argparser():
     '''
-    Return arg parser. Separated from htid2rsync_cmd For easier testing. 
+    Return arg parser. Separated from htid2rsync_cmd For easier testing.
     '''
     import argparse
     import sys
